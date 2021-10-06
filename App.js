6796aaad63f54616b9828b8afcc39289d49fc590
Jaymian-Lee <jaymian-lee@ziggo.nl>
2021-10-06 16:33:00 +0200

    Beginning

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Onboarding from './src/components/views/Onboarding'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
-      <Text> kaze saus </Text>
+      <Onboarding />
       <StatusBar style="auto" />
     </View>
   );
